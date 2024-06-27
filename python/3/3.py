31e09effb966c628b97014168c77dfeb88d211ff
Cory Owens <cory.r.owens@gmail.com>
2024-06-27 12:49:04 -0700

    Completed problem 3 in Python.

--- python/3/3.py
+++ python/3/3.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 
 def factorize(x):
-    primes = []
-    multiples = {}
-    for n in range(2, x):
-        if not multiples.get(n):
-            # Has not been marked as a multiple of a lower prime.
-            # Thus, prime.
-            primes.append(n)
-            print(f'Prime: {n}')
-            mult = n
-            while mult < x:
-                multiples[mult] = True
-                print(f'Mult: {mult}')
-                mult = mult + n
+    prime_factors = []
+
+    while x % 2 == 0:
+        prime_factors.append(2)
+        x = x // 2
+
+    for i in range(3, int(x**0.5) + 1, 2):
+        while x % i == 0:
+            prime_factors.append(i)
+            x = x // i
+
+    if x > 2:
+        prime_factors.append(x)
+
+    return prime_factors
 
 if __name__ == '__main__':
     factors = factorize(600_851_475_143)
     print(factors)
-
